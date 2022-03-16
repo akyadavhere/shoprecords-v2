afb3209669df369b6c0a57bf3618530b3d747689
Amar Yadav <akyadavhere@gmail.com>
2022-03-16 18:31:29 +0530

    changed min-view-port to 950px

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ import ErrorMessage from "./components/ErrorMessage"
 
 export default function App() {
 
-	const requiredWidth = "(min-width: 1080px)"
+	const requiredWidth = "(min-width: 950px)"
 
 	const [isRequiredWidth, setIsRequiredWidth] = useState(window.matchMedia(requiredWidth).matches)
 	window.addEventListener("resize",() => setIsRequiredWidth(window.matchMedia(requiredWidth).matches))
