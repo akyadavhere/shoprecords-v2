e2095d9ae4ac4e8bd14620d6585f757f36625185
Amar Yadav <akyadavhere@gmail.com>
2022-03-16 19:31:21 +0530

    added message for desktop mode switching

--- src/components/ErrorMessage.js
+++ src/components/ErrorMessage.js
@@ -3,11 +3,11 @@ import { isMobile } from "react-device-detect"
 
 export default function ErrorMessage(props) {
    return (
-      <div className="d-flex flex-row justify-content-center pt-3">
+      <div className="d-flex flex-column align-items-center pt-3">
          <p> {props.message} </p>
          {
             (props.mobileMessage && isMobile) ? 
-            <p> {props.mobileMessage} </p>
+            <p style={{"fontSize":"10px"}}> ({props.mobileMessage}) </p>
             : null  
          }
       </div>
